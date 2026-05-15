f05a070bd2e394e2e511daa7220b535be6388868
Hacker41 <199835595+Ryann-4@users.noreply.github.com>
2026-05-14 20:44:53 -0500

    32

--- api/server.js
+++ api/server.js
@@ -291,25 +291,7 @@ app.use((req, res, next) => {
         "Access-Control-Allow-Methods", 
         "GET, POST, PUT, DELETE, OPTIONS, PATCH"
     );
-    res.header(
-        "Access-Control-Allow-Headers", `
-            Content-Type, 
-            Authorization, 
-            uploadedby, 
-            ngrok-skip-browser-warning, 
-            x-admin-password, 
-            fileId, 
-            chunkIndex, 
-            totalChunks, 
-            filename, 
-            x-user-id, 
-            X-File-Id, 
-            X-Chunk-Number, 
-            X-Total-Chunks, 
-            X-Filename, 
-            X-User-Id
-        `
-    );
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, uploadedby, ngrok-skip-browser-warning, x-admin-password, fileId, chunkIndex, totalChunks, filename, x-user-id, X-File-Id, X-Chunk-Number, X-Total-Chunks, X-Filename, X-User-Id");
     if (req.method === "OPTIONS") return res.sendStatus(200);
     next();
 });
